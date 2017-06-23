769eab5fb0e9611bec1d200204ad3fac88299871
teenthofabud <anirbansifler@gmail.com>
2017-06-24 03:23:15 +0530

    Added test class for upload controller

--- src/test/java/com/cadence/upload/test/controller/UploadControllerTest.java
+++ src/test/java/com/cadence/upload/test/controller/UploadControllerTest.java
@@ -1,34 +1,77 @@
 package com.cadence.upload.test.controller;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.cadence.upload.controller.UploadController;
+import com.cadence.upload.model.FileModel;
+import com.cadence.upload.model.FileUploadMetadataModel;
+import com.cadence.upload.service.UploadService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@WebMvcTest(UploadController.class)
+@TestPropertySource("classpath:test.properties")
 public class UploadControllerTest {
-
-	@LocalServerPort
-    private int port;
 	
 	@Autowired
-	private UploadController uploadController;
+	private MockMvc mockMvc;
+
+	@MockBean
+	private UploadService uploadService;
 	
-	 @Autowired
-	 private TestRestTemplate testRestTemplate;
-	 
-	 @Test
-	 public void hj() {
-		 
-	 }
+	@Autowired
+	private ObjectMapper objectMapper; 
+
+	@Value("${file.chunk.size}")
+	private Integer fileChunkSize;
+
+	@Value("${baseDir}")
+	private String baseDir;
 	
+	@Value("#{new java.lang.Long('${random.range.lower}')}")
+	private Long lower;
+	
+	@Value("#{new java.lang.Long('${random.range.upper}')}")
+	private Long upper;
+	
+	@Test
+	public void shouldReturnFileUploadMetadataWhenBaseDirIsGiven() throws Exception {
+		FileModel fm1 = new FileModel();
+		fm1.setLastModified(System.currentTimeMillis());
+		fm1.setName("file1");
+		fm1.setSize(ThreadLocalRandom.current().nextLong(lower, upper));
+		FileModel fm2 = new FileModel();
+		fm2.setLastModified(System.currentTimeMillis());
+		fm2.setName("file2");
+		fm1.setSize(ThreadLocalRandom.current().nextLong(lower, upper));
+		List<FileModel> filesInBaseDir = new ArrayList<>();
+		filesInBaseDir.add(fm2);
+		filesInBaseDir.add(fm1);
+		FileUploadMetadataModel uploadMetadata = new FileUploadMetadataModel();
+		uploadMetadata.setFilesInBaseDir(filesInBaseDir);
+		uploadMetadata.setFileChunkSize(fileChunkSize);
+		String jsonString = objectMapper.writeValueAsString(uploadMetadata);
+		when(uploadService.getCompletelyUploadedFiles(baseDir)).thenReturn(filesInBaseDir);
+		mockMvc.perform(get("/upload/metadata/" + baseDir)).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().json(jsonString));
+	}
+
 }
