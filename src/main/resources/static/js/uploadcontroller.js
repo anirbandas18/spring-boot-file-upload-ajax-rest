05876a5ed2c8d61181154c29576456a7953c65d4
teenthofabud <anirbansifler@gmail.com>
2017-07-07 02:32:55 +0530

    Added se[arate sprt order icon change dynamically

--- src/main/resources/static/js/uploadcontroller.js
+++ src/main/resources/static/js/uploadcontroller.js
@@ -1,38 +1,43 @@
 /**
  * 
  */
-app.controller('uploadcontroller', function($location, $scope, $window) {
-	var file1 = {
-		name : "Introduction to R programming.mp4",
-		progress : 25,
-		status : "InProgress",
-		size : 4868729,
-		uploadedOn : Date.now()
-	};
-	var file2 = {
-		name : "Introduction to machine learning_ Alex Simola.pdf",
-		progress : 100,
-		status : "Completed",
-		size : 2898689878,
-		uploadedOn : Date.now()
-	};
-	var file3 = {
-		name : "Machine Learning Resource Guide 2014.pdf",
-		progress : 0,
-		status : "TBD",
-		size : 48728,
-		uploadedOn : Date.now()
-	};
-	$scope.fileList = [ file1, file2, file3 ];
-	$scope.reverseSort = false;
-	$scope.orderByField = function(x) {
-		$scope.customField = x;
-		$scope.reverseSort = !$scope.reverseSort;
-	}
-	$scope.selectRow = function(file) {
-		console.log(file.name);
-	};
-	var map = $location.search();
-	sessionStorage.setItem('baseDir', map.baseDir);
-	$scope.baseDir = sessionStorage.getItem('baseDir');
-});
+app
+		.controller(
+				'uploadcontroller',
+				function($location, $scope, $window) {
+					var file1 = {
+						name : "Introduction to R programming.mp4",
+						progress : 25,
+						status : "InProgress",
+						size : 4868729,
+						uploadedOn : Date.now()
+					};
+					var file2 = {
+						name : "Introduction to machine learning_ Alex Simola.pdf",
+						progress : 100,
+						status : "Completed",
+						size : 2898689878,
+						uploadedOn : Date.now()
+					};
+					var file3 = {
+						name : "Machine Learning Resource Guide 2014.pdf",
+						progress : 0,
+						status : "TBD",
+						size : 48728,
+						uploadedOn : Date.now()
+					};
+					$scope.fileList = [ file1, file2, file3 ];
+					$scope.propertyName = 'name';
+					$scope.reverse = true;
+					$scope.orderByField = function(propertyName) {
+						$scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse
+								: false;
+						$scope.propertyName = propertyName;
+					}
+					$scope.selectRow = function(file) {
+						console.log(file.name);
+					};
+					var map = $location.search();
+					sessionStorage.setItem('baseDir', map.baseDir);
+					$scope.baseDir = sessionStorage.getItem('baseDir');
+				});
