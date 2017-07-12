b6a098bf3c2a7582d90c985086f96c7b1d538e27
anirban <anirban@PC-ANIRBAN.global.cadence.com>
2017-07-12 17:41:50 +0530

    Modified model factory to directly parse JSON object

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -13,28 +13,10 @@ app.controller('operationscontroller', function($scope, $location){
 
 app.controller('fileuploadcontroller',
 				function($scope, FileModel) {
-					var file1 = new FileModel();
-					file1.name = "Introduction to R programming.mp4";
-					file1.progress = 25;
-					file1.status = "InProgress";
-					file1.size = 4868729;
-					file1.lastModified = 1499376984778;
-					/*var file2 = {
-						name : "Introduction to machine learning_ Alex Simola.pdf",
-						progress : 100,
-						status : "Completed",
-						size : 2898689878,
-						lastModified : 1498376984778
-					};
-					var file3 = {
-						name : "Machine Learning Resource Guide 2014.pdf",
-						progress : 0,
-						status : "TBD",
-						size : 48728,
-						lastModified : 1497376984778
-					};*/
-					console.log(file1.toString());
-					$scope.uploadedFileList = [ file1 ];
+					var f1 = {name : "Introduction to R programming.mp4", size : 4868729, lastModified : 1499376984778, progress : 25, status : "InProgress"};
+					var f2 = {name : "Introduction to machine learning_ Alex Simola.pdf", size : 2898689878, lastModified : 1498376984778, progress : 100, status : "Completed"};
+					var f3 = {name : "Machine Learning Resource Guide 2014.pdf", size : 48728, lastModified : 1497376984778, progress : 0, status : "TBD"};
+					$scope.uploadedFileList = [ new FileModel(f1), new FileModel(f2), new FileModel(f3)  ];
 					$scope.propertyName = 'name';
 					$scope.reverse = true;
 					$scope.orderByField = function(propertyName) {
@@ -44,8 +26,9 @@ app.controller('fileuploadcontroller',
 					}
 					$scope.onFileSelection = function () {
 						for(var i = 0 ; i < $scope.selectedFileList.length ; i++ ) {
-							var file = $scope.selectedFileList[i];
-							console.log(file);
+							var fileModel = new FileModel($scope.selectedFileList[i]);
+							console.log(fileModel);
+							$scope.uploadedFileList.push(fileModel);
 						}
 					};
 					$scope.selectRow = function(file) {
--- src/main/resources/static/js/factory.js
+++ src/main/resources/static/js/factory.js
@@ -2,12 +2,12 @@
  * 
  */
 app.factory('FileModel', function() {
-	function FileModel() {
-		this.name = new String();
-		this.progress = new Number();
-		this.status = new String();
-		this.size = new Number();
-		this.lastModified = new Number();
+	function FileModel(file) {
+		this.name = file.name;
+		this.size = file.size
+		this.lastModified = file.lastModified;
+		this.progress = typeof file.progress == "undefined" ? 0 : file.progress;// default = 0
+		this.status = typeof file.progress == "undefined" ? "InProgress" : file.status;// default = "InProgress"
 	};
 	FileModel.prototype.toString = function () {
 		return JSON.stringify(this);
